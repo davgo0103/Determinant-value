a8a7c50096d1969f640aaa837764a5e8108ba79c
davgo0103 <davvgo@gmail.com>
2022-04-01 14:46:45 +0800

    初始化

--- Determinant-value/Determinant-value.cpp
+++ Determinant-value/Determinant-value.cpp
@@ -1,9 +1,13 @@
 ﻿#include <iostream>
 #include <iomanip>
+#include <fstream>
+#include <string>
+
 using namespace std;
-void swap(double** p, int* tx, int* ty, int* m, int* n, int* err) {
+void swap(double** p, int* tx, int* ty, int* m, int* n) {
     double tmp;
     int tmpx = *tx, tmpy = *ty;
+sw:
     for (int i = tmpy; i < *m; i++) {
         if (p[i][tmpx] != 0) {
             for (int s = tmpx; s < *n; s++) {
@@ -14,11 +18,11 @@ void swap(double** p, int* tx, int* ty, int* m, int* n, int* err) {
             return;
         }
     }
-    if (*m != 1 && *n / 2 != 1) {
-        *err = 1;
-        return;
+    tmpx++;
+    *tx = *tx + 1;
+    if (tmpx != *n && tmpy != *m) {
+        goto sw;
     }
-
 }
 void up(double** p, int* tx, int* ty, int* m, int* n) {
     int tmpx = *tx, tmpy = *ty;
@@ -44,21 +48,9 @@ void down(double** p, int* tx, int* ty, int* m, int* n) {
         }
     }
 }
-void print(double** p, int m, int n) {
-    for (int i = 0; i < m; i++) {
-        for (int j = n / 2; j < n; j++) {
-            cout << setw(2) << p[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-void operation(double** p, int* tx, int* ty, int* m, int* n, int* err) {
+void operation(double** p, int* tx, int* ty, int* m, int* n) {
     int tmpy = *ty;
     double leader = p[tmpy][*tx];
-    if (leader == 0) {
-        *err = 1;
-        return;
-    }
     if (leader != 0) {
         for (int i = *tx; i < *n; i++) { //取得前導1
             if (leader != 1) {
@@ -72,51 +64,77 @@ void operation(double** p, int* tx, int* ty, int* m, int* n, int* err) {
     }
     *ty = *ty + 1; *tx = *tx + 1;
 }
-int main()
-{
-    int m, n, x = 0, y = 0, err = 0;
-    double** matrix;
-    cout << "Please enter the matrix size(m*n):\nm=";
-    cin >> m;
-    cout << "n=";
-    cin >> n;
-    n = n * 2;
-    matrix = (double**)malloc(m * sizeof(double*));
-    cout << "Please enter a matrix:" << endl;
-    for (int i = 0; i < m; i++)
-    {
-        matrix[i] = (double*)malloc(n * sizeof(double));
-        for (int j = 0; j < n / 2; j++)
-        {
-            cin >> matrix[i][j];
+void print(double** p, int m, int n) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << setw(2) << p[i][j] << " ";
         }
-        for (int j = n / 2; j < n; j++) {
-            if ((n / 2 + i) == j) {
-                matrix[i][j] = 1;
-            }
-            else {
-                matrix[i][j] = 0;
-            }
+        cout << endl;
+    }
+}
+void openfile(int* m, int* n) {
+    ifstream ifs;
+    string buffer;
+    string space_delimiter = " ";
+    size_t pos = 0;
+    ifs.open("D:\input.txt");
+    if (!ifs.is_open()) {
+        cout << "Failed to open file.\n";
+    }
+    else {
+        while (getline(ifs, buffer)) {
+            *m = *m + 1;
+        }
+        while ((pos = buffer.find(space_delimiter)) != string::npos) { //計算n
+            buffer.erase(0, pos + space_delimiter.length());
+            *n = *n + 1;
         }
 
+        ifs.close();
     }
+}
+
+int main()
+{
+    ifstream ifs;
+    string buffer;
+    string space_delimiter = " ";
+    size_t pos = 0;
+    int m = 0, n = 1, x = 0, y = 0;
+    double** matrix = NULL;
+
+    openfile(&m, &n);
+
+    ifs.open("D:\input.txt");
+    if (!ifs.is_open()) {
+        cout << "Failed to open file.\n";
+    }
+    else {
+        matrix = (double**)malloc(m * sizeof(double*));
+        int i = 0;
+        while (getline(ifs, buffer)) {
+            matrix[i] = (double*)malloc(n * sizeof(double));
+            int j = 0;
+            while ((pos = buffer.find(space_delimiter)) != string::npos) { //計算n
+                matrix[i][j] = stof(buffer.substr(0, pos));
+                buffer.erase(0, pos + space_delimiter.length());
+                j++;
+            }
+            i++;
+        }
+        ifs.close();
+    }
+
+
     while (true)
     {
         if (matrix[y][x] == 0 && x < n && y < m) { //找出非零整數
-            swap(*&matrix, &x, &y, &m, &n, &err);
+            swap(*&matrix, &x, &y, &m, &n);
         }
-        operation(*&matrix, &x, &y, &m, &n, &err);
-        if (x + 1 > n || y + 1 > m || err == 1) {
+        operation(*&matrix, &x, &y, &m, &n);
+        if (x + 1 > n || y + 1 > m) {
             break;
         }
-        print(*&matrix, m, n);
     }
-    cout << "\n\nAns:\n";
-    if (err == 1) {
-        cout << "Inverse matrix error\n\n";
-    }
-    else {
-        print(*&matrix, m, n);
-    }
-
+    print(*&matrix, m, n);
 }
